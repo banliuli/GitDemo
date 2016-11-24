1ed8cac0add56c954ec134bc8e9481eea213d4e8
banliuli <1342215280@qq.com>
2016-11-24 09:46:04 +0800

    实现了登录和注册页面之间的跳转，button改为圆角

--- MyApplication/app/src/main/java/com/example/administrator/suishouji/RegisterPage.java
+++ MyApplication/app/src/main/java/com/example/administrator/suishouji/RegisterPage.java
@@ -1,13 +1,46 @@
 package com.example.administrator.suishouji;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterPage extends AppCompatActivity {
+
+    private Button fh;
+    private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.register_page);
+
+        initId();
+        setListener();
+    }
+
+    private void  initId(){
+        fh =(Button)findViewById(R.id.btn_register_page_fh);
+        register =(Button)findViewById(R.id.btn_register_page_register);
+    }
+
+    private void setListener(){
+        MyListener listener = new MyListener();
+        fh.setOnClickListener(listener);
+    }
+
+    private class MyListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+
+            Intent i= new Intent();
+            switch (v.getId()){
+                case R.id.btn_register_page_fh:
+                    i.setClass(RegisterPage.this,LoginPage.class);
+                    break;
+            }
+            startActivity(i);
+        }
     }
 }
