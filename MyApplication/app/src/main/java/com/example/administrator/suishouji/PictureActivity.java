4fda75b4e3ee7dd1e9e5c075bed17713e3f095c6
zhangruoyu <849030106@qq.com>
2016-11-24 10:01:35 +0800

    完成了我的图片、我的文本、用户调查、关于随手记四个页面的UI设计，并且实现了关于页面与设置页面的来回跳转。

--- MyApplication/app/src/main/java/com/example/administrator/suishouji/PictureActivity.java
+++ MyApplication/app/src/main/java/com/example/administrator/suishouji/PictureActivity.java
@@ -1,14 +1,14 @@
 package com.example.administrator.suishouji;
 
 import android.app.Activity;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class PictureActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_picture);
     }
 }
--- MyApplication/app/src/main/java/com/example/administrator/suishouji/SettingActivity.java
+++ MyApplication/app/src/main/java/com/example/administrator/suishouji/SettingActivity.java
@@ -7,13 +7,14 @@ import android.view.View;
 import android.widget.TextView;
 
 public class SettingActivity extends Activity {
-    private TextView mTv_conceal,mTv_opinion;
+    private TextView mTv_conceal,mTv_opinion,mTv_about;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_setting);
         mTv_conceal=(TextView)findViewById(R.id.Tv_conceal);
         mTv_opinion=(TextView)findViewById(R.id.Tv_opinion);
+        mTv_about=(TextView)findViewById(R.id.Tv_about);
 
         mTv_conceal.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +27,13 @@ public class SettingActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Intent i=new Intent(SettingActivity.this,OpinionActivity.class);
+                startActivity(i);
+            }
+        });
+        mTv_about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(SettingActivity.this,AboutActivity.class);
                 startActivity(i);
             }
         });
