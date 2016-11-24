58966ba8949208e57601223604a98ed707103de3
jry1995 <2827168820@qq.com>
2016-11-24 08:37:14 +0800

    编辑的三个页面的UI及页面之间的跳转代码

---
+++ MyApplication/app/src/main/java/com/example/administrator/suishouji/EditHomeActivity.java
@@ -0,0 +1,74 @@
+package com.example.administrator.suishouji;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class EditHomeActivity extends AppCompatActivity {
+
+    private Button BtnCollect;
+    private Button BtnMove;
+    private Button BtnEdit;
+    private Button BtnMore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edithome);
+
+        //获取界面控件
+        getView();
+        //注册监听事件
+        setListener();
+    }
+
+    private void getView() {
+        BtnCollect = (Button) findViewById(R.id.btn_collect);
+        BtnMove = (Button) findViewById(R.id.btn_move);
+        BtnEdit = (Button) findViewById(R.id.btn_edit);
+        BtnMore = (Button) findViewById(R.id.btn_more);
+    }
+
+    private void setListener() {
+        MyListener listener = new MyListener();
+        BtnCollect.setOnClickListener(listener);
+        BtnMove.setOnClickListener(listener);
+        BtnEdit.setOnClickListener(listener);
+        BtnMore.setOnClickListener(listener);
+    }
+
+    private class MyListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_move:
+                    Intent intent = new Intent();
+                    intent.setClass(getApplicationContext(),MoveActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.btn_edit:
+                    Intent intent2 = new Intent();
+                    intent2.setClass(getApplicationContext(),EditActivity.class);
+                    startActivity(intent2);
+                    break;
+                case R.id.btn_more:
+                    PopupMenu popupMenu = new PopupMenu(getApplicationContext(),v);
+                    MenuInflater inflater = popupMenu.getMenuInflater();
+                    //添加单击事件
+                    popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+                            return false;
+                        }
+                    });
+                    break;
+            }
+        }
+    }
+}
---
+++ MyApplication/app/src/main/java/com/example/administrator/suishouji/MoveActivity.java
@@ -0,0 +1,42 @@
+package com.example.administrator.suishouji;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MoveActivity extends AppCompatActivity {
+
+    private ImageView IvBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move);
+
+        //获取界面控件
+        getView();
+        //注册监听事件
+        setListener();
+    }
+
+    private void getView() {
+        IvBack = (ImageView) findViewById(R.id.Iv_back);
+    }
+
+    private void setListener() {
+        IvBack.setOnClickListener(backListener);
+    }
+
+    //“返回”点击事件
+    View.OnClickListener backListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent();
+            intent.setClass(getApplication(),EditHomeActivity.class);
+            startActivity(intent);
+        }
+    };
+}
