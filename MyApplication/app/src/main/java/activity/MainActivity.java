4c2a6d03c6ab525d397c70d85d58349041274ec0
wangranrrr <270824537@qq.com>
2016-11-28 17:26:12 +0800

    "我的收藏"页面的布局完善

--- MyApplication/app/src/main/java/activity/MainActivity.java
+++ MyApplication/app/src/main/java/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package activity;
 
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,7 +12,7 @@ import android.widget.Button;
 import com.example.administrator.suishouji.R;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private Button bianji;
     private Button mine;
