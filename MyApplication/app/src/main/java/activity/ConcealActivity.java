bacd4f1073e32ff0563ecc80eaefa447d4a4e993
wangranrrr <270824537@qq.com>
2016-12-12 11:39:09 +0800

    完善“密码保护”开关按钮设置密码后显示被选中状态的问题

--- MyApplication/app/src/main/java/activity/ConcealActivity.java
+++ MyApplication/app/src/main/java/activity/ConcealActivity.java
@@ -1,7 +1,9 @@
 package activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CompoundButton;
@@ -9,10 +11,17 @@ import android.widget.ImageView;
 import android.widget.ToggleButton;
 
 import com.example.administrator.suishouji.R;
+import com.example.administrator.suishouji.ToggleStatus;
 
 public class ConcealActivity extends Activity {
     private ImageView mIv_back;
     private ToggleButton mTogBtn;
+    ToggleStatus status = new ToggleStatus();
+
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+    private boolean first;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,44 +29,84 @@ public class ConcealActivity extends Activity {
         setContentView(R.layout.layout_conceal);
         getView();
         MyListener();
-
-
+        setData();
         mTogBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
-                // TODO Auto-generated method stub
-                if(isChecked){
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                {
                     mTogBtn.setBackgroundResource(R.drawable.switch_btn);
-                    Intent i=new Intent(ConcealActivity.this,ConcealPwd.class);
-                    startActivity(i);
-
-
-                }else{
-                    //未选中
+                    Intent i1 = new Intent(ConcealActivity.this, ConcealPwd.class);
+                    startActivity(i1);
+                    status.setOne(isChecked);
+                }
+                else{
                     mTogBtn.setBackgroundResource(R.drawable.switch_btn);
+                    status.setOne(false);
                 }
             }
         });// 添加监听事件
     }
-    private void getView(){
-        mIv_back=(ImageView)findViewById(R.id.img_activity_conceal_back);
+    private void getView() {
+        mIv_back = (ImageView) findViewById(R.id.img_activity_conceal_back);
         mTogBtn = (ToggleButton) findViewById(R.id.mTogBtn); // 获取到控件
     }
-    private void MyListener(){
-        MyListener listener=new MyListener();
+    private void MyListener() {
+        MyListener listener = new MyListener();
         mIv_back.setOnClickListener(listener);
+        mTogBtn.setOnClickListener(listener);
     }
-
-    private class MyListener implements View.OnClickListener{
+    private class MyListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
-            switch (v.getId()){
+            switch (v.getId()) {
                 case R.id.img_activity_conceal_back:
-                    Intent i=new Intent(ConcealActivity.this,SettingActivity.class);
+                    Intent i = new Intent(ConcealActivity.this, SettingActivity.class);
                     startActivity(i);
                     break;
             }
         }
     }
-
+    private void setData(){
+        preferences = getSharedPreferences("togglebuttonstatus", Context.MODE_PRIVATE);
+		/*
+		 * 判断是不是第一次运行该程序
+		 * （因为第一次运行时，SharedPreferences是没有保存"first"的，
+		 * "first"不存在即为null，默认返回自己设置的参数true）
+		 *
+		 */
+        first = preferences.getBoolean("first", true);
+        editor = preferences.edit();
+        if(first){
+            getStatus();
+        }
+        else{
+            status.one = preferences.getBoolean("s_one", false);
+            setToggButonStatus(status);
+        }
+    }
+    /*
+	 * 根据保存的参数设置每个ToggleButton的状态
+	 */
+    private void setToggButonStatus(ToggleStatus data){
+        mTogBtn.setChecked(data.one);
+    }
+    /*
+	 * 获取每个ToggleButton的状态，并保存在status里面
+	 */
+    private void getStatus(){
+        status.one = mTogBtn.isChecked();
+    }
+    @Override
+    protected void onStop() {
+        // TODO Auto-generated method stub
+        super.onStop();
+        if(first){
+            editor.putBoolean("first", false);
+        }
+        //关闭之前把数据写进去
+        editor.putBoolean("s_one", status.one);
+        editor.commit();
+    }
 }
+
---
+++ MyApplication/app/src/main/java/com/example/administrator/suishouji/ToggleStatus.java
@@ -0,0 +1,14 @@
+package com.example.administrator.suishouji;
+
+/**
+ * Created by lenovo on 2016/12/8.
+ */
+public class ToggleStatus {
+    public boolean one;
+    public boolean isOne() {
+        return one;
+    }
+    public void setOne(boolean one) {
+        this.one = one;
+    }
+}
